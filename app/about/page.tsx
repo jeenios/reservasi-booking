d05678960535e618f4438025410774ae7b1f91ea
Ali Latukau <alilatukau07@gmail.com>
2025-11-13 23:15:05 +0700

    fix metadata

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,6 +1,12 @@
 import Image from "next/image";
 import HeaderSection from "@/components/header-section";
 import { IoEyeOutline, IoLocateOutline } from "react-icons/io5";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "About",
+  description: "Who we are",
+};
 
 const AboutPage = () => {
   return (
